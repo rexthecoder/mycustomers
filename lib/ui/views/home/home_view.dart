2742803a982be322c1c456ec387af8c71986092e
isaacadariku <isaacadariku05@gmail.com>
2020-06-17 13:07:18 +0100

    modify home

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:stacked/stacked.dart';
+
+import 'home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      builder: (context, model, child) => PlatformScaffold(
+        body: SafeArea(
+            child: Container(
+          child: Center(
+            child: Text('BUILD'),
+          ),
+        )),
+      ),
+      viewModelBuilder: () => HomeViewModel(),
+    );
+  }
+}
