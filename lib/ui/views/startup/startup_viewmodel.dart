375015c89c606b7a2af58fecfc5941fd5ad0d5c5
isaacadariku <isaacadariku05@gmail.com>
2020-06-17 13:04:35 +0100

    adding startup & home view

---
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:mycustomers/app/locator.dart';
+import 'package:mycustomers/app/router.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class StartupViewModel extends BaseViewModel {
+  
+  final NavigationService _navigationService = locator<NavigationService>();
+  
+  // TODO:
+  //    loading,
+  //    authentication,
+  //    permissions manager,
+  
+  Future setup() async {
+    await Future.value();
+    await _navigationService.replaceWith(Routes.homeViewRoute);
+  }
+}
