46ef7642593c41a9c8675329a0e0a13cada7b6e3
ogunwoleoluwatobi <oluwatstar21@gmail.com>
2020-06-19 00:05:09 +0100

    Name and Company Screen Done

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mycustomers/ui/views/home/home_view.dart';
-import 'package:mycustomers/ui/views/signup/signup_view.dart';
 import 'package:mycustomers/ui/views/startup/startup_view.dart';
 
 abstract class Routes {
@@ -20,7 +19,7 @@ class Router {
         );
         case Routes.homeViewRoute:
         return CupertinoPageRoute<dynamic>(
-          builder: (context) => SignUpView(),
+          builder: (context) => HomeView(),
           settings: settings,
         );
       default:
