df9b2a5a0223c4850623df2fe84c954f5f501664
isaacadariku <isaacadariku05@gmail.com>
2020-06-19 03:59:43 +0100

    onboarding view model

--- lib/ui/views/onboarding/onboarding_viewmodel.dart
+++ lib/ui/views/onboarding/onboarding_viewmodel.dart
@@ -1,3 +1,16 @@
+import 'dart:ui';
+
+import 'package:mycustomers/core/data_sources/onboarding_data.dart';
 import 'package:stacked/stacked.dart';
 
-class OnboardingViewModel extends BaseViewModel {}
+class OnboardingViewModel extends BaseViewModel {
+
+  Color _color = onboardData[0].accentColor;
+
+  Color get color => _color;
+
+  set color(Color color) {
+    _color = color;
+    notifyListeners();
+  }
+}
