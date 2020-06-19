36cf8cf371337aa9e6df2c4f1e21774800048e13
isaacadariku <isaacadariku05@gmail.com>
2020-06-19 02:05:42 +0100

    fixing component

--- lib/ui/views/onboarding/onboarding_view.dart
+++ lib/ui/views/onboarding/onboarding_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:mycustomers/core/data_sources/onboarding_data.dart';
 import 'package:stacked/stacked.dart';
 
 import 'onboarding_viewmodel.dart';
@@ -8,17 +9,67 @@ import 'onboarding_viewmodel.dart';
 class OnboardingView extends StatelessWidget {
 
   final PageController pageController = PageController();
-  
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<OnboardingViewModel>.reactive(
-      builder: (context, model, child) => PlatformScaffold(
-        body: SafeArea(
-            child: Container(
-          child: Center(
-            child: Text('Welcome Developer \n You Can BUILD HERE'),
+      builder: (context, model, child) => Stack(
+      children: <Widget>[
+        PageView.builder(
+          controller: pageController,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: onboardData.length,
+          itemBuilder: (context, index) {
+            return OnboardPage(
+              pageController: pageController,
+              pageModel: onboardData[index],
+            );
+          },
+        ),
+        Container(
+          width: double.infinity,
+          height: 70,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 32.0),
+                  child: Text(
+                    'fun with',
+                    style: Theme.of(context).textTheme.title.copyWith(
+                          color: colorProvider.color,
+                        ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: colorProvider.color,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 80.0, left: 40),
+            child: PageViewIndicator(
+              controller: pageController,
+              itemCount: onboardData.length,
+              color: colorProvider.color,
+            ),
+          ),
+        )
+      ],
       ),
       viewModelBuilder: () => OnboardingViewModel(),
     );
