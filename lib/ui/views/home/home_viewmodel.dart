41466079f33465c8ef87b6385fbeb5a9c9c2777d
Chinaza <ogwo.chinaza@yahoo.com>
2020-06-19 12:15:52 +0100

    added home view

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,3 +1,13 @@
 import 'package:stacked/stacked.dart';
 
-class HomeViewModel extends BaseViewModel {}
+class HomeViewModel extends BaseViewModel {
+  String _title = 'Home View';
+  String get title => _title;
+
+  final double _amount = 100.86;
+  double get amount => _amount;
+
+  void btnViewDetails() {
+    print('hello');
+  }
+}
