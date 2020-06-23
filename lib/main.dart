dab8e7a1e7f635d2ae5fb68bb71bb981752e4f65
Becca-Saka <Ree4sure@gmail.com>
2020-06-23 22:35:12 +0100

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:device_preview/device_preview.dart';  
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
 import 'package:stacked_services/stacked_services.dart';
 import './ui/shared/themes.dart' as themes;
 
