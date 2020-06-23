c8a913d8edb9b54001d3a5e43c17b98866514ada
isaacadariku <isaacadariku05@gmail.com>
2020-06-23 01:12:20 +0100

    change new onboarding image

--- lib/ui/views/home/onboarding/onboarding_view.dart
+++ lib/ui/views/home/onboarding/onboarding_view.dart
@@ -1,22 +1,135 @@
-import 'package:fancy_on_boarding/fancy_on_boarding.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 
 import 'package:stacked/stacked.dart';
-
 import 'onboarding_viewmodel.dart';
+import 'package:flutter_screenutil/size_extension.dart';
+
+// @override
+// void initState() {
+//   super.initState();
+//   Timer.periodic(Duration(seconds: 5), (Timer timer) {
+//     if (_currentPage < 2) {
+//       _currentPage++;
+//     } else {
+//       _currentPage = 0;
+//     }
+
+//     _pageController.animateToPage(
+//       _currentPage,
+//       duration: Duration(milliseconds: 350),
+//       curve: Curves.easeIn,
+//     );
+//   });
+// }5
 
 class OnboardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
+    ScreenUtil.init(context, width: w, height: h);
+
     return ViewModelBuilder<OnboardingViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        body: FancyOnBoarding(
-          doneButtonText: "Let's Go",
-          skipButtonText: "Skip",
-          pageList: pageList,
-          onDoneButtonPressed: () => model.setup(),
-          onSkipButtonPressed: () => model.setup(),
+        body: Container(
+          height: h,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              PageView(
+                onPageChanged: model.onChangedFunction,
+                controller: model.pageController,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/onboarding/onboarding1.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/onboarding/onboarding2.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/onboarding/onboarding3.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: h/2 - 100,
+                // left: 150.h,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Indicator(
+                      positionIndex: 0,
+                      currentIndex: model.currentIndex,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Indicator(
+                      positionIndex: 1,
+                      currentIndex: model.currentIndex,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Indicator(
+                      positionIndex: 2,
+                      currentIndex: model.currentIndex,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 50.h,
+                              child: InkWell(
+                  onTap: () {
+                    model.nextFunction();
+                  },
+                  child: Container(
+                    height: 45.h,
+                    width: 300.w,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Color(0xFF333CC1)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Get Started',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       viewModelBuilder: () => OnboardingViewModel(),
@@ -24,55 +137,19 @@ class OnboardingView extends StatelessWidget {
   }
 }
 
-//list of PageModel to be set on the onBoarding Screens
-final pageList = [
-  PageModel(
-      color: const Color(0xFF45ADA7),
-      heroAssetPath: 'assets/images/onboarding/onboarding1.png',
-      title: Text('MyCustomer',
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Color(0xFFFDA741),
-            fontSize: 36.sp,
-          )),
-      body: Text('Track your debts',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF1D6AFF),
-            fontSize: 18.sp,
-          )),
-      iconAssetPath: ''),
-  PageModel(
-      color: const Color(0xFF3DBED0),
-      heroAssetPath: 'assets/images/onboarding/onboarding2.jpg',
-      title: Text('MyCustomer',
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Color(0xFFFDA741),
-            fontSize: 36.sp,
-          )),
-      body: Text('Market your Business',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF1D6AFF),
-            fontSize: 18.sp,
-          )),
-      iconAssetPath: ''),
-  PageModel(
-    color: const Color(0xFF7ED8CA),
-    heroAssetPath: 'assets/images/onboarding/onboarding3.jpg',
-    title: Text('MyCustomer',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          color: Color(0xFFFDA741),
-          fontSize: 36.sp,
-        )),
-    body: Text('Manage your business',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Color(0xFF1D6AFF),
-          fontSize: 18.sp,
-        )),
-    iconAssetPath: '',
-  ),
-];
+class Indicator extends StatelessWidget {
+  final int positionIndex, currentIndex;
+  const Indicator({this.currentIndex, this.positionIndex});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 12,
+      width: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          color:
+              positionIndex == currentIndex ? Colors.white : Colors.transparent,
+          borderRadius: BorderRadius.circular(100)),
+    );
+  }
+}
--- lib/ui/views/home/onboarding/onboarding_viewmodel.dart
+++ lib/ui/views/home/onboarding/onboarding_viewmodel.dart
@@ -1,17 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:mycustomers/app/locator.dart';
 import 'package:mycustomers/app/router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 
-
 class OnboardingViewModel extends BaseViewModel {
+ 
+ PageController pageController;
+  int currentIndex = 0;
+
+  static const _kDuration = const Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
+
+  nextFunction() {
+    pageController.nextPage(duration: _kDuration, curve: _kCurve);
+    notifyListeners();
+  }
+
+  previousFunction() {
+    pageController.previousPage(duration: _kDuration, curve: _kCurve);
+    notifyListeners();
+  }
+
+  onChangedFunction(int index) {
+    currentIndex = index;
+    notifyListeners();
+  }
+
+
   final NavigationService _navigationService = locator<NavigationService>();
 
+  
   Future setup() async {
     await Future.value();
     await _navigationService.clearStackAndShow(Routes.languageViewRoute);
   }
-
 
 }
