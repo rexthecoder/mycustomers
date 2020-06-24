1280df05a96da15c8b34bf5ff784ddac2aa267dc
isaacadariku <isaacadariku05@gmail.com>
2020-06-24 02:55:06 +0100

    change screen util init

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/screenutil.dart'; 
 import 'package:stacked_services/stacked_services.dart';
 import './ui/shared/themes.dart' as themes;
 
@@ -23,7 +24,19 @@ void main() {
 
 class App extends StatelessWidget {
   @override
+  
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
+   double height = MediaQuery.of(context).size.height;
+
+     /* 
+    1. Setup for screenutil, only needed once here
+    2. screen resolution (in px) according to phone screen
+    3. Import screen util to all views
+    4. use in this manner E.g Container(width: 50.w, height:200.h)
+    */
+    ScreenUtil.init(context, width: width, height: height, allowFontScaling: true);
 
   // Status bar
   SystemChrome.setSystemUIOverlayStyle(
