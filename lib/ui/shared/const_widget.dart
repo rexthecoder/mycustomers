267e1529f94328f8266445f79902428a8a3a4ca1
olumidayy <niolumi4eva@gmail.com>
2020-06-24 15:39:46 +0100

    Completed marketing_home_page view

--- lib/ui/shared/const_widget.dart
+++ lib/ui/shared/const_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 Widget btnDesign(String btnText, Color textColor, Color bgColor) {
   return Container(
@@ -18,5 +19,43 @@ Widget btnDesign(String btnText, Color textColor, Color bgColor) {
           fontWeight: FontWeight.bold,
           fontSize: 16,
         )),
+  );
+}
+
+Widget notificationBell(bool isNewNotification) {
+  String notification = isNewNotification ? 'notification' : 'no_notification';
+  final String assetName = "assets/icons/$notification.png";
+  final Widget notificationIcon = Image.asset(assetName);
+  return notificationIcon;
+}
+
+Widget arrow(bool forward) {
+  String direction = forward ? 'foward' : 'back';
+  final String assetName = "assets/icons/arrow_$direction.svg";
+  final Widget svgIcon = SvgPicture.asset(assetName,
+      color: Color(0xff333cc1),
+      semanticsLabel: 'An arrow');
+  return svgIcon;
+}
+
+
+Widget circleDesign(double outerRadius, double innerRadius){
+  return Container(
+    height: outerRadius * 2,
+    width: outerRadius * 2,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(outerRadius),
+      color: Color(0xff1d6aff),
+    ),
+    child: Center(
+      child: Container(
+        height: innerRadius * 2,
+        width: innerRadius * 2,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(outerRadius),
+          color: Color(0xff333cc1),
+        ),
+      ),
+    ),
   );
 }
