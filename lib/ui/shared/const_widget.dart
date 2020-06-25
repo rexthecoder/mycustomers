6a5bb72ae7fe563ce9c764d9b1ccea8820244282
isaacadariku <isaacadariku05@gmail.com>
2020-06-25 12:17:40 +0100

    added a background widget

--- lib/ui/shared/const_widget.dart
+++ lib/ui/shared/const_widget.dart
@@ -45,3 +45,77 @@ Widget btnHome(String btnText, context) {
     ),
   );
 }
+
+class HomeBackgrouWidget extends StatelessWidget {
+  const HomeBackgrouWidget({
+    Key key,
+    @required this.height,
+    @required this.config,
+    @required this.width,
+    @required this.widget,
+    @required this.context,
+  }) : super(key: key);
+
+  final double height;
+  final SizeConfig config;
+  final double width;
+  final BuildContext context;
+  final Widget widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: height * 0.8,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/onboarding/ob1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: config.yMargin(context, 20)),
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+              color: Color(0xffffffff),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(height * 0.05),
+                topRight: Radius.circular(height * 0.05),
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: config.yMargin(context, 7)),
+                Text(
+                  'LANGUAGE',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: config.yMargin(context, 4),
+                  ),
+                ),
+                SizedBox(height: config.yMargin(context, 4)),
+                Text(
+                  'Select your language',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: config.yMargin(context, 3),
+                  ),
+                ),
+                SizedBox(height: config.yMargin(context, 0.2)),
+                Container(
+                    height: config.yMargin(context, 40),
+                    width: config.xMargin(context, 90),
+                    child: widget),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
