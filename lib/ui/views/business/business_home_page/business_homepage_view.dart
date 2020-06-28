187f1f9896d6ffd227ae9c6a559539a7af5c1e06
Jonyango <jeanadagi@gmail.com>
2020-06-28 19:38:18 +0200

    Added the setting page It still needs fine tuning

--- lib/ui/views/business/business_home_page/business_homepage_view.dart
+++ lib/ui/views/business/business_home_page/business_homepage_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mycustomers/ui/views/business/profile/profile_page_view.dart';
+// import 'package:mycustomers/ui/views/business/profile/profile_page_view.dart';
+import 'package:mycustomers/ui/views/business/settings/settings_view.dart';
 // import 'package:mycustomers/ui/widgets/shared/under_construction.dart';
 import 'package:stacked/stacked.dart';
 
@@ -12,7 +13,7 @@ class BusinessHomePageView extends StatelessWidget {
     return ViewModelBuilder<BusinessHomePageViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: SafeArea(
-            child: ProfilePageView(),
+            child:SettingsHomePageView(),
       ),
       ),
       viewModelBuilder: () => BusinessHomePageViewModel(),
