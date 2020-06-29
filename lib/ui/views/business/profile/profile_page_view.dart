ce7e937b7cce35f4c05cb18fd18f075c00c2e740
Demilade <demiladeoladugba@gmail.com>
2020-06-29 09:42:33 +0100

    linked profile to menu bar removed elevation from appbar

--- lib/ui/views/business/profile/profile_page_view.dart
+++ lib/ui/views/business/profile/profile_page_view.dart
@@ -7,8 +7,6 @@ import 'package:stacked/stacked.dart';
 import 'package:mycustomers/ui/shared/const_text_style.dart';
 import 'package:flutter_screenutil/size_extension.dart';
 
-
-
 class ProfilePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,77 +15,66 @@ class ProfilePageView extends StatelessWidget {
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
           backgroundColor: ThemeColors.background,
-          centerTitle:true,
+          centerTitle: true,
           elevation: 0.0,
-          title:Text(model.title,
-          style:kheadingText,
+          title: Text(
+            model.title,
+            style: kheadingText,
           ),
-          iconTheme: IconThemeData(
-            color: BrandColors.secondary),
-         
+          iconTheme: IconThemeData(color: BrandColors.secondary),
         ),
-      
-        
         body: SafeArea(
-            child: Padding(
-            padding: EdgeInsets.symmetric(horizontal:24.w),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
             child: Container(
-              decoration: BoxDecoration(
-                border:Border(top: BorderSide(
-                  color:ThemeColors.gray.shade700
-                ))
-              ),
               child: Stack(
-               children: <Widget>[
-                 Align(
-                   alignment: Alignment.topCenter,
-                   child: Container(
-                     child: CircleAvatar(
-                       backgroundColor: ThemeColors.gray.shade800,
-                          radius: 80.h,
-                          child:GestureDetector(
-                            onTap:(){},
-                            child:Icon(
-                              Icons.add_a_photo,
-                              color: ThemeColors.gray.shade600,
-                              size:40.h,
-                            ) ,)
-                     ),
-                   )),
-              
+                children: <Widget>[
                   Align(
-                    alignment: Alignment.center,
-                    child: ProfilePartialBuildsView()),
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        child: CircleAvatar(
+                            backgroundColor: ThemeColors.gray.shade800,
+                            radius: 80.h,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Icon(
+                                Icons.add_a_photo,
+                                color: ThemeColors.gray.shade600,
+                                size: 40.h,
+                              ),
+                            )),
+                      )),
+                  Align(
+                      alignment: Alignment.center,
+                      child: ProfilePartialBuildsView()),
                   Align(
                     alignment: Alignment.bottomCenter,
-                      child: Container(
-                        height: 105.h,
+                    child: Container(
+                      height: 105.h,
                       alignment: Alignment.center,
-                      margin: EdgeInsets.only(bottom:20.h),
+                      margin: EdgeInsets.only(bottom: 20.h),
                       decoration: BoxDecoration(
-                        borderRadius:BorderRadius.circular(8.sp),
-                        color: BrandColors.secondary
-                        
-                      ),
-                      child:FlatButton(
-                        onPressed:(){
-                          //TODO Implement the action to be completed once the user clicks the save button
-                        } ,
-                        child: Text(
-                          model.btnText,
-                          textAlign: TextAlign.center,
-                          style:TextStyle(
-                            color:ThemeColors.background,
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 32.sp,
-                          )
-                        )) ,),
+                          borderRadius: BorderRadius.circular(8.sp),
+                          color: BrandColors.secondary),
+                      child: FlatButton(
+                          onPressed: () {
+                            //TODO Implement the action to be completed once the user clicks the save button
+                          },
+                          child: Text(model.btnText,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: ThemeColors.background,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 32.sp,
+                              ))),
+                    ),
                   )
-                ],),
-            ),
+                ],
               ),
-      ),
+            ),
+          ),
+        ),
       ),
       viewModelBuilder: () => ProfilePageViewModel(),
     );
