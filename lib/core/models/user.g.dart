906733d227509de4cef72fdff028ab4c6d5e7100
ufe-pr <frenzy591@gmail.com>
2020-06-30 12:14:14 +0100

    Refactor

--- lib/core/models/user.g.dart
+++ lib/core/models/user.g.dart
@@ -8,12 +8,20 @@ part of 'user.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) {
   return User(
-    json['id'] as String,
-    json['email'] as String,
+    id: json['id'] as String,
+    email: json['email'] as String,
+    firstName: json['firstName'] as String,
+    lastName: json['lastName'] as String,
+    userRole: json['userRole'] as String,
+    phone: json['phone'],
   );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
+      'firstName': instance.firstName,
+      'lastName': instance.lastName,
+      'userRole': instance.userRole,
+      'phone': instance.phone,
     };
