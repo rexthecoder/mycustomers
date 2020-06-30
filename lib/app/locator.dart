f5c7a62a8eb39741ca0a72d2e8c57d54c6bb18aa
isaacadariku <isaacadariku05@gmail.com>
2020-06-30 03:34:45 +0100

    added services

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -1,5 +1,9 @@
 import 'package:get_it/get_it.dart';
+import 'package:mycustomers/core/services/auth/auth_service.dart';
+import 'package:mycustomers/core/services/auth/auth_service_impl.dart';
 import 'package:mycustomers/core/services/customer_services.dart';
+import 'package:mycustomers/core/services/http/http_service.dart';
+import 'package:mycustomers/core/services/http/http_service_impl.dart';
 import 'package:mycustomers/core/services/page_service.dart';
 import 'package:mycustomers/core/services/storage_util_service.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -8,10 +12,28 @@ final GetIt locator = GetIt.instance;
 
 const bool USE_MOCK_CUSTOMER = true;
 
-void setupLocator() {
-  locator.registerLazySingleton(() => NavigationService());
-  locator.registerSingletonAsync<IStorageUtil>(() => SharedStorageUtil.getInstance());
-  locator.registerLazySingleton(() => PageService());
-  locator.registerLazySingleton<ICustomerService>(() => USE_MOCK_CUSTOMER ? MockCustomerService() : CustomerService());
-
+/// Setup function that is run before the App is run.
+///   - Sets up singletons that can be called from anywhere
+/// in the app by using locator<Service>() call.
+///   - Also sets up factor methods for view models.
+Future<void> setupLocator() async {
+  // Services
+  locator.registerLazySingleton(
+    () => NavigationService(),
+  );
+  locator.registerSingletonAsync<IStorageUtil>(
+    () => SharedStorageUtil.getInstance(),
+  );
+  locator.registerLazySingleton(
+    () => PageService(),
+  );
+  locator.registerLazySingleton<ICustomerService>(
+    () => USE_MOCK_CUSTOMER ? MockCustomerService() : CustomerService(),
+  );
+  locator.registerLazySingleton<AuthService>(
+    () => AuthServiceImpl(),
+  );
+  locator.registerLazySingleton<HttpService>(
+    () => HttpServiceImpl(),
+  );
 }
