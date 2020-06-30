a3ef4caba68eac8fa99029c5de1326a7443bb857
isaacadariku <isaacadariku05@gmail.com>
2020-06-30 14:52:43 +0100

    refactor

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -2,7 +2,6 @@ import 'package:mycustomers/app/locator.dart';
 import 'package:mycustomers/app/router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-import 'package:mycustomers/core/services/permissions.dart';
 
 class StartupViewModel extends BaseViewModel {
   final NavigationService _navigationService = locator<NavigationService>();
