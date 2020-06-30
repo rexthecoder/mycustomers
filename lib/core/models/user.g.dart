3f4f04d10d5cfd0365d19b56ed2bb78fe552684d
ufe-pr <frenzy591@gmail.com>
2020-06-30 04:08:05 +0100

    Rebuild user model part file

--- lib/core/models/user.g.dart
+++ lib/core/models/user.g.dart
@@ -8,16 +8,12 @@ part of 'user.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) {
   return User(
-    json['id'] as int,
+    json['id'] as String,
     json['email'] as String,
-    json['customer'] == null
-        ? null
-        : Customer.fromJson(json['customer'] as Map<String, dynamic>),
   );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
-      'customer': instance.customer?.toJson(),
     };
