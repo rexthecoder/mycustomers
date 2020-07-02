410b140038b82f72c9a4e6a27d75ba3545591a7a
isaacadariku <isaacadariku05@gmail.com>
2020-07-02 16:49:45 +0100

    implemented business view and model

--- lib/ui/views/home/signup/business/business_viewmodel.dart
+++ lib/ui/views/home/signup/business/business_viewmodel.dart
@@ -1,5 +1,5 @@
 import 'package:mycustomers/app/locator.dart';
-import 'package:mycustomers/app/router.dart';
+import 'package:mycustomers/ui/views/main/main_view.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -7,7 +7,17 @@ class BusinessViewModel extends BaseViewModel {
   final NavigationService _navigationService = locator<NavigationService>();
 
   Future<void> navigateToNext() async {
-    await Future.delayed(Duration(seconds: 2));
-    await _navigationService.replaceWith(Routes.mainViewRoute);
+    // await Future.delayed(Duration(seconds: 2));
+    await _navigationService.replaceWithTransition(MainView(),
+        opaque: true, transition: 'fade', duration: Duration(seconds: 1));
+
   }
+
+bool btnColor = true;
+
+   void activeBtn() {
+    btnColor = !btnColor;
+    notifyListeners();
+  }
+
 }
