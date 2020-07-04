69c2a87f715e91a8690c8483d7f96732de101d8a
isaacadariku <isaacadariku05@gmail.com>
2020-07-04 18:45:58 +0100

    comment

--- lib/core/services/sms_services.dart
+++ lib/core/services/sms_services.dart
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import 'dart:async';
-import 'package:sms/sms.dart';
-
-  class SmsService {
-  SmsReceiver _receiver = new SmsReceiver();
-  SmsService() {
-    _receiver.onSmsReceived.listen(handler);
-}
-     // ignore: close_sinks
-     final _otpController = StreamController<SmsMessage>();
-void handler(SmsMessage message) {
-   // ignore: unused_local_variable
-   var add = _otpController.sink.add(message);
-}
-}
-
-
-
-=======
 // import 'dart:async';
 // import 'package:sms/sms.dart';
 
@@ -32,4 +12,3 @@ void handler(SmsMessage message) {
 //     _otpController.add(message);
 //   }
 // }
->>>>>>> upstream/master
