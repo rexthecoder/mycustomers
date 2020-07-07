1686c37140adaee8d408236ed4b237a4706e9ec8
ogunwoleoluwatobi <oluwatstar21@gmail.com>
2020-07-07 21:17:53 +0100

    Fixed Notification View

---
+++ lib/ui/views/home/notification_view/notification_viewmodel.dart
@@ -0,0 +1,21 @@
+import 'package:stacked/stacked.dart';
+
+class NotificationViewModel extends BaseViewModel {
+  List notifications = [
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'},
+    {'msg':'A reminder was set for Invoice 007 debt (receivable) due on 30/06/2020', 'price': '3000', 'time': '10:30AM'}
+  ];
+}
