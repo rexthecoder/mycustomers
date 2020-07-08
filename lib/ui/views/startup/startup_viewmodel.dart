ed2a5e14393688e864e67b08270830aa7816c9a2
ufe-pr <ufe-pr@users.noreply.github.com>
2020-07-08 04:43:54 +0100

    Update startup_viewmodel.dart

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -23,7 +23,7 @@ class StartupViewModel extends BaseViewModel {
 
   Future setup() async {
     await locator.allReady();  
-    await  Future.delayed(Duration(seconds: 1));
+//    await  Future.delayed(Duration(seconds: 1));
     if (await checkLoggedIn()) _navigationService.replaceWith(Routes.mainViewRoute);
     else _navigationService.replaceWith(Routes.onboardingViewRoute);
   }
