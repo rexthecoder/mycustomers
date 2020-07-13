24dc173d7e3e4510d9552b9d97daee9f7353cac5
isaacadariku <isaacadariku05@gmail.com>
2020-07-13 19:46:11 +0100

    Remove unsed files

--- lib/core/services/transaction/repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hive/hive.dart';
-
-abstract class Repository<T extends HiveObject> {
-  
-  Future<void> addTransaction(T newObject);
-  
-  Future<List<T>> getTransactions(int id);
-}
