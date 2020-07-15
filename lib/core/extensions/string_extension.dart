a82da1ec9c11d6ec033e718ff6c405971044713d
Demilade <demiladeoladugba@gmail.com>
2020-07-15 23:50:20 +0100

    fixed index error

--- lib/core/extensions/string_extension.dart
+++ lib/core/extensions/string_extension.dart
@@ -1,5 +1,8 @@
 ///Created by Demilade Oladugba on 6/19/2020
 
 extension StringExtension on String {
-  String get capitalize => '${this[0].toUpperCase()}${this.substring(1).toLowerCase()}';
+  String get capitalize {
+    if (this == null || this.isEmpty) return this;
+    return '${this[0].toUpperCase()}${this.substring(1).toLowerCase()}';
+  }
 }
