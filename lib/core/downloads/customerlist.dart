3ae5fd8ecd68515b5a01fe6c0739a3a94cdc31f0
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-07-15 08:06:54 +0530

    customerlist pdf according to design

--- lib/core/downloads/customerlist.dart
+++ lib/core/downloads/customerlist.dart
@@ -1,5 +1,5 @@
 
-import 'package:newapp/Downloads/storagepermission.dart';
+import 'storagepermission.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
