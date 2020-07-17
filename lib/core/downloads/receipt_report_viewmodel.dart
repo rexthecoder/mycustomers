cb95665dcb4a2a05c5b18c2f0001daba9a8fbe36
Ademola Babs <demolafadumo@gmail.com>
2020-07-17 03:14:54 +0100

    Update Debtors Transaction Receipt Report PDF.

--- lib/core/downloads/receipt_report_viewmodel.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:mycustomers/ui/shared/const_color.dart';
-import 'package:stacked/stacked.dart';
-
-class ReceiptReportViewModel extends BaseViewModel {}
