a9b4b3405ded33ecb67a31604f1c80d2d8a19965
ufe-pr <ufe-pr@users.noreply.github.com>
2020-07-26 02:20:21 +0100

    ..

--- lib/ui/views/business/profile/edit_profile/edit_profile_viewmodel.dart
+++ lib/ui/views/business/profile/edit_profile/edit_profile_viewmodel.dart
@@ -72,7 +72,7 @@ class EditProfileViewModel extends BaseViewModel {
 
  Uint8List dataFromBase64String(String base64String){
    return base64Decode(base64String);
- } 
+ }
 
   String base64String(Uint8List data){
     return base64Encode(data);
