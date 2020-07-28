dfb8340e51460866a8984df92beaaf83f7fff070
ufe-pr <ufe-pr@users.noreply.github.com>
2020-07-28 10:19:41 +0100

    Fix pin screens

--- lib/ui/views/business/settings/enter_pin_page/enter_pin_viewmodel.dart
+++ lib/ui/views/business/settings/enter_pin_page/enter_pin_viewmodel.dart
@@ -4,32 +4,26 @@ import 'package:mycustomers/core/services/password_manager_services.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-
 class EnterPinViewModel extends BaseViewModel {
-   final PasswordManagerService _passwordManagerService =locator<PasswordManagerService>();
+  final PasswordManagerService _passwordManagerService =
+      locator<PasswordManagerService>();
 
   final NavigationService _navigationService = locator<NavigationService>();
 
+  Future<bool> onOldPinCompleted(String value) async {
+    String passFrmDb = await _passwordManagerService
+        .getPassword(); // get the password stored in the db
+    bool check = passFrmDb == value;
 
-
- Future<bool> onOldPinCompleted(String value) async{
-   String passFrmDb= await _passwordManagerService.getPassword(); // get the password stored in the db
-   bool check = passFrmDb == value;
-
-   if(check){
-     _navigationService.back();
-   } else {
-     _passwordManagerService.showUnmatchedPinErrorMessage();
-   }
-   return check;
- }
-
-
- void clearValueIfPinsDoNotMatch(TextEditingController textEditingController){
-   textEditingController.clear();
-
+    if (check) {
+      _navigationService.back();
+    } else {
+      _passwordManagerService.showUnmatchedPinErrorMessage();
     }
+    return check;
+  }
 
+  void clearValueIfPinsDoNotMatch(TextEditingController textEditingController) {
+    textEditingController.clear();
+  }
 }
-
-
