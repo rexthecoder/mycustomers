bbba428dafebe0990fcf89d3d53c6de23495401b
Demilade <demiladeoladugba@gmail.com>
2020-07-29 12:19:43 +0100

    refactored support page

--- lib/ui/views/business/business_support_page/support_page_viewmodel.dart
+++ lib/ui/views/business/business_support_page/support_page_viewmodel.dart
@@ -1,22 +1,14 @@
 import 'package:stacked/stacked.dart';
 
 class SupportPageViewModel extends BaseViewModel {
-  
-  String _item = 'Subject';
-  String get item => _item;
-
   List<String> _items = [
-    //TODO: Add Dropdown Items
-    'first',
-    'second', 'third'
+    'Enquires',
+    'Suggestions',
+    'Bug Fixes',
+    'Other',
   ];
 
   List<String> get items => _items;
-
-  void setItem(String item){
-    _item = item;
-    notifyListeners();
-  }
 
   validateFields(value) => value.isEmpty ? 'Please fill' : null;
 }
