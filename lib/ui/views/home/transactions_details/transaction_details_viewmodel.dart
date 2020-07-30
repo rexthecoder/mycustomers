fed644d2c7196cd73599d315a80b0c48b2b090a2
ayshabintmahmud <aysherbintmahmud@gmail.com>
2020-07-30 01:37:05 +0100

    Share screenshot for transaction details page

--- lib/ui/views/home/transactions_details/transaction_details_viewmodel.dart
+++ lib/ui/views/home/transactions_details/transaction_details_viewmodel.dart
@@ -7,6 +7,7 @@ import 'package:mycustomers/core/data_sources/transaction/transaction_local_data
 import 'package:mycustomers/core/models/hive/customer_contacts/customer_contact_h.dart';
 import 'package:mycustomers/core/models/hive/transaction/transaction_model_h.dart';
 import 'package:mycustomers/core/services/customer_contact_service.dart';
+import 'package:mycustomers/core/services/permission_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:wc_flutter_share/wc_flutter_share.dart';
@@ -19,6 +20,7 @@ class TransactionDetailsViewModel extends ReactiveViewModel{
   final _transactionService = locator<TransactionLocalDataSourceImpl>();
   CustomerContact get contact => _customerContactService.contact;
   TransactionModel get transaction => _transactionService.stransaction;
+  final PermissionService _permissionService = locator<IPermissionService>();
 
   File imageFile;
 
@@ -37,22 +39,45 @@ class TransactionDetailsViewModel extends ReactiveViewModel{
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_transactionService, _customerContactService];
 
-  Future<void> shareTextandImage() async {
+  Future<void> shareImageAndText() async {
     try {
-       final Uint8List bytes =await imageFile.readAsBytes();
+      final Uint8List bytes = await imageFile.readAsBytes();
       final String fileName =
-          '${contact.name}-Transactiondetail.pdf';
-      //final ByteData bytes = await rootBundle.load('assets/images/app_icon.png');
+          '${contact.name}-Transactiondetail.png';
+
       await WcFlutterShare.share(
-          sharePopupTitle: 'Share',
-          subject: contact.name,
-          text: 'Transaction with ${contact.name}',
-          fileName: fileName,
-          mimeType: 'file/pdf',
-          bytesOfFile: bytes.buffer.asUint8List());
+        sharePopupTitle: 'Share Your Business Card',
+        subject: contact.name,
+        text: 'Transaction with ${contact.name}',
+        fileName: fileName,
+        mimeType: 'image/png',
+        bytesOfFile: bytes.buffer.asUint8List(),
+      );
     } catch (e) {
-      Logger.e(e.toString());
-      throw Exception("Unable to share file");
+      Logger.e(e);
+      throw Exception("Unable to save image");
+    }
+  }
+
+
+  Future<void> downloadImage() async {
+    try {
+      if (await _permissionService.getStoragePermission()) {
+        final Uint8List bytes = await imageFile.readAsBytes();
+
+        final String internalStorage = '/storage/emulated/0/myCustomer';
+
+        final String fileName =
+            '${contact.name}-Transactiondetail.png';
+
+        bool isDirExist = await Directory(internalStorage).exists();
+        if (!isDirExist) Directory(internalStorage).create();
+        String tempPath = '$internalStorage/$fileName';
+        File(tempPath).writeAsBytes(bytes);
+      }
+    } catch (e) {
+      Logger.e(e);
+      throw Exception("Unable to download image");
     }
   }
 
