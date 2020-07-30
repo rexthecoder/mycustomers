9fcc5b7279dddb0a75c57fa8e495ca427cddffbc
Becca-Saka <Ree4sure@gmail.com>
2020-07-30 23:12:59 +0100

    edit profile view

--- lib/ui/views/business/profile/edit_profile/edit_profile_view.dart
+++ lib/ui/views/business/profile/edit_profile/edit_profile_view.dart
@@ -13,56 +13,160 @@ class EditProfileView extends HookWidget {
   @override
   Widget build(BuildContext context) {
 
+//    return ViewModelBuilder<EditProfileViewModel>.reactive(
+//      builder: (context, model, child) {
+//        return Scaffold(
+//          resizeToAvoidBottomInset: false,
+//          appBar: customizeAppBar(context, 0,
+//              children: [
+//                FlatButton(
+//                  child: Text('Save'),
+//                  onPressed: model.save,
+//                ),
+//              ],
+//              title: AppLocalizations.of(context).editProfile,
+//              arrowColor: BrandColors.primary,
+//              backgroundColor: Theme.of(context).backgroundColor),
+//          body: SafeArea(
+//            child: SingleChildScrollView(
+//              child: Center(
+//                child: Column(
+//                  crossAxisAlignment: CrossAxisAlignment.center,
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: <Widget>[
+//                    Row(
+//                      mainAxisAlignment: MainAxisAlignment.center,
+//                      children: <Widget>[
+//                        _previewImage(context, model),
+//                      ],
+//                    ),
+//                    EditField(
+//                      fieldName: 'Store name',
+//                    ),
+//                    EditField(
+//                      fieldName: 'Phone number',
+//                    ),
+//                    CustomRaisedButton(
+//                      txtColor: ThemeColors.background,
+//                      btnColor: ThemeColors.error,
+//                      borderColor: ThemeColors.error,
+//                      btnText: AppLocalizations.of(context).signOut,
+//                      child: Container(),
+//                      onPressed: () {
+//                        model.save();
+//                        flusher(AppLocalizations.of(context).save, context);
+//                      },
+//                    ),
+//                    SizedBox(
+//                      height: SizeConfig.yMargin(context, 2),
+//                    )
+//                  ],
+//                ),
+//              ),
+//            ),
+//          ),
+//        );
+//      },
+//      viewModelBuilder: () => EditProfileViewModel(),
+//      onModelReady: (model) {
+//        model.initt();
+//      },
+//    );
     return ViewModelBuilder<EditProfileViewModel>.reactive(
       builder: (context, model, child) {
         return Scaffold(
           resizeToAvoidBottomInset: false,
           appBar: customizeAppBar(context, 0,
               children: [
-                FlatButton(
-                  child: Text('Save'),
-                  onPressed: model.save,
+                Center(
+                  child: FlatButton(
+                    child: Text('Save'),
+                    onPressed: model.save,
+                  ),
                 ),
               ],
-              title: AppLocalizations.of(context).editProfile,
+              title: AppLocalizations.of(context).profile,
               arrowColor: BrandColors.primary,
               backgroundColor: Theme.of(context).backgroundColor),
-          body: SafeArea(
-            child: SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        _previewImage(context, model),
-                      ],
-                    ),
-                    EditField(
-                      fieldName: 'Store name',
-                    ),
-                    EditField(
-                      fieldName: 'Phone number',
-                    ),
-                    CustomRaisedButton(
-                      txtColor: ThemeColors.background,
-                      btnColor: ThemeColors.error,
-                      borderColor: ThemeColors.error,
-                      btnText: AppLocalizations.of(context).signOut,
-                      child: Container(),
-                      onPressed: () {
-                        model.save();
-                        flusher(AppLocalizations.of(context).save, context);
-                      },
-                    ),
-                    SizedBox(
-                      height: SizeConfig.yMargin(context, 2),
-                    )
-                  ],
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          _previewImage(context, model),
+                          SizedBox(width: SizeConfig.xMargin(context, 6),),
+                          Text(
+                            model.userName.isEmpty ? '' : model.userName,
+                            style: TextStyle(
+                              color: BrandColors.primary,
+                              fontSize: SizeConfig.textSize(context, 6),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: SizeConfig.xMargin(context, 5),),
+                          Icon(Icons.edit, color: Theme.of(context).brightness== Brightness.dark
+                            ?Colors.white:Colors.black,)
+                        ],
+                      ),
+                      SizedBox(height: SizeConfig.yMargin(context, 5),),
+                      EditField(
+                        fieldName:  AppLocalizations.of(context).storeName,
+                        hintText: AppLocalizations.of(context).storeName,
+                      ),
+                      Divider(),
+                      EditField(
+                        fieldName: AppLocalizations.of(context).phoneNumber,
+                        hintText: AppLocalizations.of(context).phoneNumber,
+                      ),
+                      Divider(),
+                      EditField(
+                        fieldName: AppLocalizations.of(context).emailAddress,
+                        hintText: AppLocalizations.of(context).emailAddress,
+                      ),
+                      Divider(),
+                      EditField(
+                        //TODO:  Localize Location
+                        fieldName:  'Address',
+                        hintText: 'Address',
+                      ),
+                      Divider(),
+                      EditField(
+                        //TODO:  Localize tagline
+//                        fieldName: AppLocalizations.of(context).companyTagLine,
+//                        hintText: AppLocalizations.of(context).companyTagLine,
+                        fieldName: 'Tag Line',
+                        hintText: 'Tag Line',
+                      ),
+                      Divider(),
+
+
+                    ],
+                  ),
                 ),
-              ),
+                CustomRaisedButton(
+                  txtColor: ThemeColors.background,
+                  btnColor: Colors.red,
+                  borderColor: ThemeColors.error,
+                  btnText: '',
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.power_settings_new,color: ThemeColors.background,),
+                      SizedBox(width: SizeConfig.xMargin(context, 2),),
+                      Text(AppLocalizations.of(context).signOut,style: TextStyle(
+                        color: ThemeColors.background,
+                      ),)
+                    ],
+                  ),
+                  onPressed: () {
+                    model.save();
+                    flusher(AppLocalizations.of(context).save, context);
+                  },
+                ),
+              ],
             ),
           ),
         );
@@ -129,11 +233,6 @@ class EditField extends HookViewModelWidget<EditProfileViewModel> {
 }
 
 Widget _previewImage(BuildContext context, EditProfileViewModel model) {
-//  final Text retrieveError = _getRetrieveErrorWidget(model);
-//  if (retrieveError != null) {
-//    return retrieveError;
-//  }
-//  print('here');
 
   return CircleAvatar(
     backgroundColor: ThemeColors.unselect,
@@ -145,14 +244,39 @@ Widget _previewImage(BuildContext context, EditProfileViewModel model) {
             model.userName.isEmpty ? '' : model.userName.substring(0, 1),
             style: TextStyle(
               color: BrandColors.primary,
-              fontSize: SizeConfig.textSize(context, 18),
+              fontSize: SizeConfig.textSize(context, 8),
               fontWeight: FontWeight.bold,
             ),
           )
         : Container(), // SizedBox.expand(child: Image.memory(model.currentStore.storePic, fit: BoxFit.cover)),
-    radius: 70,
+    radius: SizeConfig.xMargin(context, 10),
   );
 }
+//Widget _previewImage(BuildContext context, EditProfileViewModel model) {
+////  final Text retrieveError = _getRetrieveErrorWidget(model);
+////  if (retrieveError != null) {
+////    return retrieveError;
+////  }
+////  print('here');
+//
+//  return CircleAvatar(
+//    backgroundColor: ThemeColors.unselect,
+//    backgroundImage: model.currentStore.storePic == null
+//        ? null
+//        : MemoryImage(model.currentStore.storePic),
+//    child: model.currentStore.storePic == null
+//        ? Text(
+//            model.userName.isEmpty ? '' : model.userName.substring(0, 1),
+//            style: TextStyle(
+//              color: BrandColors.primary,
+//              fontSize: SizeConfig.textSize(context, 18),
+//              fontWeight: FontWeight.bold,
+//            ),
+//          )
+//        : Container(), // SizedBox.expand(child: Image.memory(model.currentStore.storePic, fit: BoxFit.cover)),
+//    radius: 70,
+//  );
+//}
 
 //Text _getRetrieveErrorWidget(EditProfileViewModel model) {
 //  if (model.retrieveDataError != null) {
