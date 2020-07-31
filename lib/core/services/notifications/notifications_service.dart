71f85e241091a2f5cace573162f16a3694147fb3
isaacadariku <isaacadariku05@gmail.com>
2020-07-31 06:26:41 +0100

    Feature: Implemented abstract class and impl of notification service

---
+++ lib/core/services/notifications/notifications_service.dart
@@ -0,0 +1,32 @@
+/// An abstract class for local notification service
+/// 
+/// 
+abstract class NotificationRemindersService {
+  /// Get the instance from the notification package
+  getNotificationInstance();
+
+  /// initialise the plugin.
+  void initNotifications();
+
+  dynamic pendingNotificationRequests();
+
+  /// Function to schedule a reminder at a specific time daily
+  void sendNotificationDaily(
+      {int id, String title, String body, int hour, int minute});
+
+  /// Function to schedule reminder on a specific date but just once
+  void sendNotificationOnce(int id, String title, String body, DateTime time);
+
+  /// This as the function name states, sets given properties to the notification.
+  getPlatformChannelSpecfics(int id);
+
+  /// Configuration for IOS
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload);
+
+  /// To navigate to a certain screen on click of notification for devices notification panel
+  Future onSelectNotification(String payload);
+
+  /// Function to remove reminder, in the case where well, the data was deleted. So user won't receive notification still.
+  void removeReminder(int notificationId);
+}
---
+++ lib/core/services/notifications/notifications_service_impl.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'notifications_service.dart';
+
+class NotificationRemindersServiceImpl extends NotificationRemindersService {
+  var flutterLocalNotificationsPlugin;
+
+  NotificationRemindersServiceImpl() {
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    initNotifications();
+  }
+
+  getNotificationInstance() {
+    return flutterLocalNotificationsPlugin;
+  }
+
+  void initNotifications() {
+    // Sets icon to be displayed with notification (android).
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/launcher_icon');
+
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+
+    // Intialization requires two arguments, for IOS and Android. IOS requires more , but that hasn't been set yet.
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  dynamic pendingNotificationRequests() async =>
+      await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+
+
+  void sendNotificationDaily(
+      {int id, String title, String body, int hour, int minute}) async {
+    var time = new Time(hour, minute, 0);
+
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+        id, title, body, time, getPlatformChannelSpecfics(id));
+
+    print(
+        'Notification Succesfully Scheduled for ${time.toString()} with id of $id');
+  }
+
+  void sendNotificationOnce(
+      int id, String title, String body, DateTime time) async {
+    await flutterLocalNotificationsPlugin.schedule(
+        id, title, body, time, getPlatformChannelSpecfics(id));
+
+    print(
+        'Notification Succesfully Scheduled for ${time.toString()} with id of $id');
+  }
+
+  getPlatformChannelSpecfics(int id) {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        '$id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+
+    return platformChannelSpecifics;
+  }
+
+Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    return Future.value(1);
+  }
+
+  Future onSelectNotification(String payload) async {
+    print('Notification clicked');
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    // await Navigator.pushNamed(context,
+    // screen name goes here
+    // );
+  }
+
+  void removeReminder(int notificationId) {
+    flutterLocalNotificationsPlugin.cancel(notificationId);
+    print('Notfication with id: $notificationId been removed successfully');
+  }
+}
