daa20e79a6031b1cce4fcfc2b20dea01c458d656
Demilade <demiladeoladugba@gmail.com>
2020-08-01 12:50:51 +0100

    update to about my customer

--- lib/ui/views/business/about_us/about_my_customer_page/about_my_customer_viewmodel.dart
+++ lib/ui/views/business/about_us/about_my_customer_page/about_my_customer_viewmodel.dart
@@ -8,6 +8,179 @@ class AboutMyCustomerViewModel extends BaseViewModel {
   bool isOpenMeetTheMobileTeam = false;
   bool isOpenMeetTheWebTeam = false;
 
+  List<List<Person>> leaderMap = [
+    [leader[0]],
+    [
+      leader[1],
+      leader[2],
+    ],
+    [leader[3]],
+    [
+      leader[4],
+      leader[5],
+    ],
+    [leader[6]]
+  ];
+  List<List<Person>> mobileMap = [
+    [mobile[0]],
+    [
+      mobile[1],
+      mobile[2],
+    ],
+    [mobile[3]],
+    [
+      mobile[4],
+      mobile[5],
+    ],
+    [mobile[6]]
+  ];
+  List<List<Person>> webMap = [
+    [web[0]],
+    [
+      web[1],
+      web[2],
+    ],
+    [web[3]],
+    [
+      web[4],
+      web[5],
+    ],
+    [web[6]]
+  ];
+
+  static List<Person> leader = [
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+  ];
+  static List<Person> mobile = [
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+  ];
+  static List<Person> web = [
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+    Person(
+      about:
+          'About MeLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      imageUrl: '',
+      name: 'A name',
+    ),
+  ];
+
   int currentAboutCustomerPayMe = 0;
   int currentMeetTheLeaders = 0;
   int currentMeetTheMobileTeam = 0;
@@ -42,4 +215,12 @@ class AboutMyCustomerViewModel extends BaseViewModel {
     currentAboutCustomerPayMe++;
     notifyListeners();
   }
+}
+
+class Person {
+  final String name;
+  final String about;
+  final String imageUrl;
+
+  Person({this.name, this.about, this.imageUrl});
 }
