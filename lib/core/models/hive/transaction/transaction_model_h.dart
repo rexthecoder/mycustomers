bb7cc8ef5cd15e31c1a67dd9055b026c129cbab1
ogunwoleoluwatobi <oluwatstar21@gmail.com>
2020-08-01 22:44:53 +0100

    Fix Transaction  Details Error

--- lib/core/models/hive/transaction/transaction_model_h.dart
+++ lib/core/models/hive/transaction/transaction_model_h.dart
@@ -31,4 +31,26 @@ class TransactionModel extends HiveObject{
   final String description;
 
   TransactionModel({this.tId, this.sId, this.amount, this.paid, this.duedate, this.boughtdate, this.paiddate, this.description});
+
+  TransactionModel.fromjson(Map<String, dynamic> json) : 
+    tId = json['tId'],
+    sId = json['sId'],
+    amount = json['amount'],
+    paid = json['paid'],
+    duedate = json['duedate'],
+    boughtdate = json['boughtdate'],
+    paiddate = json['paiddate'],
+    description = json['description'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['tId'] = this.tId;
+    data['sId'] = this.sId;
+    data['amount'] = this.amount;
+    data['paid'] = this.paid;
+    data['duedate'] = this.duedate;
+    data['boughtdate'] = this.boughtdate;
+    data['paiddate'] = this.paiddate;
+    data['description'] = this.description;
+  }
 }
