fe46d3158b59d8eed054d4939470033162baa717
isaacadariku <isaacadariku05@gmail.com>
2020-08-02 03:01:01 +0100

    chore: add new flare animation and construct its height

--- lib/ui/widgets/stateless/loading_animation.dart
+++ lib/ui/widgets/stateless/loading_animation.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:mycustomers/core/constants/animations.dart';
 import 'package:mycustomers/ui/shared/size_config.dart';
 
+// ignore: must_be_immutable
 class LoadingAnimation extends StatelessWidget {
+  double height = 30;
+
+  LoadingAnimation({this.height});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: SizeConfig.yMargin(context, 30),
+      height: SizeConfig.yMargin(context, height),
       child: FlareActor(
         Animations.loader,
         animation: Animations.loader_name,
